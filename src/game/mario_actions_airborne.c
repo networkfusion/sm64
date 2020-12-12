003a106bd024c361750c315a9ce958730b946d0c
Robin Jones <robin@networkfusion.co.uk>
2020-12-12 01:42:14 +0000

    fix hitwall action (for build anyway)

--- src/game/mario_actions_airborne.c
+++ src/game/mario_actions_airborne.c
@@ -1337,7 +1337,7 @@ s32 act_air_hit_wall(struct MarioState *m) {
     }
 
 #ifdef AVOID_UB
-    return;
+    return set_mario_action(m, ACT_SOFT_BONK, 0); //TODO-ultimate: is this right???
 #endif
     set_mario_animation(m, MARIO_ANIM_START_WALLKICK);
 
