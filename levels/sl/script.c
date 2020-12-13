e4a293817a8ddca61fa61521d0631d3a4b900c10
sunlitspace542 <rabbidsluis123@gmail.com>
2020-12-13 18:22:05 -0500

    re-add cracked ice block in SL where it was originally intended to go

--- levels/sl/script.c
+++ levels/sl/script.c
@@ -24,7 +24,7 @@ static const LevelScript script_func_local_1[] = {
 };
 
 static const LevelScript script_func_local_2[] = {
-    OBJECT(/*model*/ MODEL_NONE, /*pos*/  977, 1024, 2075, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvSnowMoundSpawn),
+    OBJECT(/*model*/ MODEL_NONE, 		   /*pos*/  977, 1024, 2075, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvSnowMoundSpawn),
 	OBJECT(/*model*/ MODEL_SL_CRACKED_ICE, /*pos*/ 4377, 1843, 4361, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvUnusedPoundablePlatform),
     RETURN(),
 };
