0b619d9b3fd60c367b7e2bfb586404dab22fcff8
Robin Jones <robin@networkfusion.co.uk>
2020-12-30 18:06:23 +0000

    Correct bugfix ifdef should be if

--- src/game/mario.c
+++ src/game/mario.c
@@ -1494,7 +1494,7 @@ void update_mario_health(struct MarioState *m) {
 
         // Play a noise to alert the player when Mario is close to drowning.
         if (((m->action & ACT_GROUP_MASK) == ACT_GROUP_SUBMERGED) && (m->health < 0x300)
-#ifdef BUGFIX_DROWING_SOUND_METAL
+#if BUGFIX_DROWING_SOUND_METAL
         && !((m->flags & (MARIO_METAL_CAP)) > 0)
 #endif
         ) {
