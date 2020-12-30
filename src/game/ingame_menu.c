2bce38773bb37ac29dcea75db34ed56abf562c26
Robin Jones <robin@networkfusion.co.uk>
2020-12-30 02:43:59 +0000

    Correct bugfix issue.

--- src/game/ingame_menu.c
+++ src/game/ingame_menu.c
@@ -2768,10 +2768,10 @@ void print_hud_course_complete_coins(s16 x, s16 y) {
             gCourseCompleteCoins++;
             play_sound(SOUND_MENU_YOSHI_GAIN_LIVES, gGlobalSoundSource);
 
-#ifdef BUGFIX_COURSE_COMPLETE_50_COINS
-            if (gCourseCompleteCoins % 50 == 0)
+#if BUGFIX_COURSE_COMPLETE_50_COINS
+            if (gCourseCompleteCoins % 50 == 0) {
 #else
-            if (gCourseCompleteCoins == 50 || gCourseCompleteCoins == 100 || gCourseCompleteCoins == 150)
+            if (gCourseCompleteCoins == 50 || gCourseCompleteCoins == 100 || gCourseCompleteCoins == 150) {
 #endif
                 play_sound(SOUND_GENERAL_COLLECT_1UP, gGlobalSoundSource);
                 gMarioState[0].numLives++;
