9f10cd6cd3f4a6c0c7b7bb3bd3d27e7f1e6a3799
Robin Jones <robin@networkfusion.co.uk>
2021-01-08 17:54:36 +0000

    Initialize empty instead of null.

--- tools/armips.cpp
+++ tools/armips.cpp
@@ -15046,7 +15046,7 @@ wchar_t TextFile::readCharacter()
 		contentPos++;
 		break;
 	case GUESS:
-		value = NULL;
+		value = "";
 		errorText = formatString(L"Cannot read from GUESS encoding");
 		break;
 	}
